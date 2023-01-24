7682a19553348ac3ddd7fbb84fbba3659368a4ff
Busi <pietersonbusi@gmail.com>
2023-01-24 12:12:29 +0200

    created nat gateways

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -10,11 +10,11 @@ output "vpc_id" {
     value = aws_vpc.vpc.id
 }
 
-output "pulic_subnet_az1_id" {
+output "public_subnet_az1_id" {
     value = aws_subnet.public_subnet_az1.id
 }
 
-output "pulic_subnet_az2_id" {
+output "public_subnet_az2_id" {
     value = aws_subnet.public_subnet_az2.id
 }
 
